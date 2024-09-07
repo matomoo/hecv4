00613518b532c43e155e5876e977036ae81133fa
Sxy Walker <sxywlkr@gmail.com>
2024-09-07 14:32:58 +0800

    bugging

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,11 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
 
-export default clerkMiddleware({
-  // publicRoutes: ["/", "/api/dataHeaderAntrol", "/api/dataHeaderVclaim"],
-});
+export default clerkMiddleware();
 
 export const config = {
   matcher: [
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    "/",
+    // "/",
     "/(api|trpc)(.*)",
   ],
 };
