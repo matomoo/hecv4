11eb385b49787698a9f866a2a80d360f936f6aad
Sxy Walker <sxywlkr@gmail.com>
2024-09-11 10:38:47 +0800

    bugging

--- app/hooks/useDataLaporanOperasi.tsx
+++ app/hooks/useDataLaporanOperasi.tsx
@@ -7,7 +7,7 @@ const useDataLaporanOperasi = (noRawat: string) => {
   return useQuery<SchemaDataLaporanOperasi[], Error>({
     queryKey: ["dataLaporanOperasi", noRawat],
     queryFn: async () => {
-      const response = await axios.get(`http://localhost/hec/apiv3/getByNoRawat_Operasi/${noRawat}`);
+      const response = await axios.get(`https://hec1.hijr.de/hecapiv3/getByNoRawat_Operasi/${noRawat}`);
       return response.data.response.data[0];
     },
     staleTime: 10 * 1000,
