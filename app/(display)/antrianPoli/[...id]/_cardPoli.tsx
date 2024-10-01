ae0f3bdb74ea49444bb7dc1cfa4059cf643f8654
Sxy Walker <sxywlkr@gmail.com>
2024-10-01 14:26:33 +0800

    update antrian poli

--- app/(display)/antrianPoli/[...id]/_cardPoli.tsx
+++ app/(display)/antrianPoli/[...id]/_cardPoli.tsx
@@ -12,8 +12,8 @@ const CardPoli = ({ namaPoli }: { namaPoli: string }) => {
     <div >
       <div className='text-xl text-sky-400 mb-6'>{'KLINIK MATA HASANUDDIN'}</div>
       <div className='text-2xl text-slate-400 mb-2'>{'Sementara diperiksa oleh :'}</div>
-      <div className='text-5xl text-slate-300 ml-2 mb-6'>{dataAntrianPeriksaPoli?.nm_dokter}</div>
-      <div className='text-4xl text-slate-400'>{'Pasien atas nama :'} {dataAntrianPeriksaPoli?.nm_pasien} ( {dataAntrianPeriksaPoli?.no_rkm_medis} )</div>
+      <div className='text-5xl text-slate-200 ml-2 mb-6'>{dataAntrianPeriksaPoli?.nm_dokter}</div>
+      <div className='text-4xl text-slate-300'>{'Pasien atas nama :'} {dataAntrianPeriksaPoli?.nm_pasien} ( {dataAntrianPeriksaPoli?.no_rkm_medis} )</div>
     </div>
   )
 }
